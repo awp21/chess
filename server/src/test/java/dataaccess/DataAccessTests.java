5d903015ce2c0a8ed16fbac466d70a2163de2840
Alex <awp21@byu.edu>
2024-11-01 10:43:12 -0600

    allUserTests

--- server/src/test/java/dataaccess/DataAccessTests.java
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -5,6 +5,7 @@ import model.AddPlayer;
 import model.AuthData;
 import model.GameData;
 import model.UserData;
+import org.eclipse.jetty.server.Authentication;
 import org.junit.jupiter.api.*;
 import service.AlreadyTakenException;
 import service.BadRequestException;
@@ -37,9 +38,46 @@ public class DataAccessTests {
             user.create(userMan);
             Assertions.assertInstanceOf(UserData.class,user.get(userMan.username()),"User was not added");
         } catch (DataAccessException ignore){
-
         }
     }
 
+    @Test
+    public void addUserTwiceToData(){
+        try{
+            user.create(userMan);
+            user.create(userMan);
+            assert true : "Exception for double add not thrown";
+        } catch (RuntimeException | DataAccessException ignore){
+        }
+    }
+
+    @Test
+    public void getUserFromData(){
+        try{
+            user.create(userMan);
+            UserData testUser = user.get(userMan.username());
+            Assertions.assertEquals(userMan,testUser,"User was not added");
+        } catch (DataAccessException ignore){
+        }
+    }
+
+    @Test
+    public void getFalseUserFromData(){
+        try{
+            UserData testUser = new UserData("HA","Skill","Issue");
+            Assertions.assertNull(user.get(testUser.username()),"Not found as null");
+        } catch (DataAccessException ignore){
+        }
+    }
+
+    @Test
+    public void clearUser(){
+        try{
+            user.create(userMan);
+            user.clear();
+            Assertions.assertNull(user.get(userMan.username()),"User was not deleted");
+        } catch (DataAccessException ignore){
+        }
+    }
 
 }
