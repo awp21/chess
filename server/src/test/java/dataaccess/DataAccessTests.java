59cbdf5bec0dcfc954d861d79c3c7c62b4c27b8f
Alex <awp21@byu.edu>
2024-11-01 20:38:19 -0600

    AuthTests

--- server/src/test/java/dataaccess/DataAccessTests.java
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -1,18 +1,11 @@
 package dataaccess;
 
-import dataaccess.*;
-import model.AddPlayer;
 import model.AuthData;
 import model.GameData;
 import model.UserData;
-import org.eclipse.jetty.server.Authentication;
-import org.junit.jupiter.api.*;
-import service.AlreadyTakenException;
-import service.BadRequestException;
-import service.Service;
-import service.UnauthorizedException;
 
-import java.util.*;
+import org.junit.jupiter.api.*;
+
 
 public class DataAccessTests {
     private UserDAO user;
@@ -79,5 +72,105 @@ public class DataAccessTests {
         } catch (DataAccessException ignore){
         }
     }
+
+    @Test
+    public void clearGame(){
+        try{
+            game.createGame("FunGame");
+            game.clear();
+            assert game.listAllGames().isEmpty():"Games not cleared";
+        } catch (DataAccessException ignore){
+        }
+    }
+
+    @Test
+    public void clearAuth(){
+        try{
+            AuthData testData = auth.create("Jimmy");
+            auth.clear();
+            Assertions.assertNull(auth.get(testData.authToken()),"Auth not cleared");
+        } catch (DataAccessException ignore){
+        }
+    }
+
+    @Test
+    public void addAuth(){
+        try{
+            AuthData testData = auth.create(userMan.username());
+            Assertions.assertEquals(testData, auth.get(testData.authToken()),"AuthData not added");
+        } catch (DataAccessException ignore){
+        }
+    }
+
+    @Test
+    public void addBadAuthData(){
+        try{
+            auth.create(null);
+            assert false: "null exception not caught";
+        } catch (DataAccessException | RuntimeException ignore){
+        }
+    }
+
+    @Test
+    public void getCreatedAuthData(){
+        try{
+            AuthData testData = auth.create("Simon");
+            Assertions.assertNotNull(auth.get(testData.authToken()));
+        } catch (DataAccessException | RuntimeException ignore){
+        }
+    }
+
+    @Test
+    public void getBadAuthData(){
+        try{
+            AuthData testData = auth.create("Jeffery");
+            Assertions.assertNull(auth.get(testData.username()));
+        } catch (DataAccessException | RuntimeException ignore){
+        }
+    }
+
+    @Test
+    public void logoutAuth(){
+        try{
+            AuthData testData = auth.create("Jeffery");
+            auth.deleteAuthToken(testData.authToken());
+            Assertions.assertNull(auth.get(testData.authToken()));
+        } catch (DataAccessException | RuntimeException ignore){
+        }
+    }
+
+    @Test
+    public void logoutBadAuthToken(){
+        try{
+            AuthData testData = auth.create("Jeffery");
+            auth.deleteAuthToken(testData.username());
+            Assertions.assertNotNull(auth.get(testData.authToken()));
+        } catch (DataAccessException | RuntimeException ignore){
+        }
+    }
+
+    @Test
+    public void listGames(){
+        try{
+            game.createGame("Jim");
+            game.createGame("Jeb");
+            Assertions.assertFalse(game.listAllGames().isEmpty(),"Games not added");
+        } catch (DataAccessException | RuntimeException ignore){
+        }
+    }
+
+    @Test
+    public void listNoGames(){
+        try{
+            Assertions.assertTrue(game.listAllGames().isEmpty(),"Games not added");
+        } catch (DataAccessException | RuntimeException ignore){
+        }
+    }
+
+
+
+
+
+
 
 }
